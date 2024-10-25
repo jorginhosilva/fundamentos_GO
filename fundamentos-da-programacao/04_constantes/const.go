26eb0c0adb32d542fd04b99b2dd8793054c1eb6d
André Luiz <ma650696@gmail.com>
2024-10-24 21:15:58 -0300

    Constantes

---
+++ fundamentos-da-programacao/04_constantes/const.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+const (
+	x = 10 
+	y = 15
+	z = 20
+)
+
+func main() {
+	// Constantes são de valores imutáveis
+
+	// Reproduzindo uma constante na tela 
+	fmt.Println(x, y, z)
+
+}
