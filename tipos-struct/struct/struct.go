fde4294f21dbca4424033c669cf81fc7eb60007b
André Luiz <ma650696@gmail.com>
2024-11-29 21:24:11 -0300

    ajustando um código

--- tipos-struct/struct/struct.go
+++ tipos-struct/struct/struct.go
@@ -2,17 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
-	// criando uma struct
-	// também uma forma de organizar seus dados
-	type Pessoa struct { // declara um novo tipo, chamado Pessoa.
+// criando uma struct
+// também uma forma de organizar seus dados
+type Pessoa struct { // declara um novo tipo, chamado Pessoa.
 		// Nome, Idade e Endereco -> são os campos da struct. Cada campo tem um tipo específico 
 		Nome 		string
 		Idade		int
 		Endereco	string	
 	}	
 
-	pessoa1 := Pessoa{"André", 29, "Rua Oton Gaspar de Farias, 47"}
+func main() {
+	
+	pessoa1 := Pessoa{"André", 28, "Rua Oton Gaspar de Farias, 47"}
 
 	fmt.Println(pessoa1.Nome)
 	fmt.Println(pessoa1.Idade)
