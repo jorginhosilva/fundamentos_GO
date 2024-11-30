f9e8f5ec94fd1580395f6fcb65beba37b6afc6da
André Luiz <ma650696@gmail.com>
2024-11-29 21:23:19 -0300

    respondendo exercicios

--- exercicios/exercicios_nivel5/ex001/ex001.go
+++ exercicios/exercicios_nivel5/ex001/ex001.go
@@ -1,5 +1,35 @@
 package main
 
+import "fmt"
+
+type Pessoa struct {
+	Nome				string
+	Sobrenome			string
+	SaboresFavoritos 	[]string
+}
+
 func main() {
-	
+	pessoa1 := Pessoa{
+		Nome: "Graça",
+		Sobrenome: "Abreu",
+		SaboresFavoritos: []string{"Chocolate", "Granola", "Morango", "Aveia"},
+	}
+
+	pessoa2 := Pessoa{
+		Nome: "André",
+		Sobrenome: "Abreu",
+		SaboresFavoritos: []string{"Pitaia", "Chocolate", "Negresco", "Cajú"},
+	}
+
+	pessoas := []Pessoa{pessoa1, pessoa2}
+
+	for _, pessoa := range pessoas {
+		fmt.Printf("Nome: %s\n", pessoa.Nome)
+		fmt.Println("Sabores favoritos:")
+		for _, sabor := range pessoa.SaboresFavoritos {
+			fmt.Printf("- %s\n", sabor)
+		}
+		fmt.Println()
+	}
+
 }
