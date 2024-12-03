4a03082c690c79e03a6c51a47e92a4dd24eec3e8
André Luiz <ma650696@gmail.com>
2024-12-02 21:09:39 -0300

    Tentando finalizar uns exercícios

--- exercicios/exercicios_nivel5/ex002/ex002.go
+++ exercicios/exercicios_nivel5/ex002/ex002.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
-	
+import "fmt"
+
+type Pessoa struct {
+	Nome				string
+	Sobrenome			string
+	SaboresFavoritos 	[]string
 }
+
+func main() {
+	mappessoas := make(map[string]Pessoa)
+
+	mappessoas["Abreu"] = Pessoa{
+		Nome: "Graça",
+		Sobrenome: "Abreu",
+		SaboresFavoritos: []string{"Chocolate", "Granola", "Morango", "Aveia"},
+	}
+
+	mappessoas["Ferreira"] = Pessoa{
+		Nome: "André",
+		Sobrenome: "Ferreira",
+		SaboresFavoritos: []string{"Pitaia", "Chocolate", "Negresco", "Cajú"},
+	}
+	fmt.Println("\n")
+	for _, valorpessoas := range mappessoas {
+		fmt.Printf("Meu nome é: %s\n", valorpessoas.Nome)
+		fmt.Printf("Os meus sorvetes favoritos são: %s\n", valorpessoas.SaboresFavoritos)
+	}
+	fmt.Println("\n")
+}
+
+// Demonstre os valores do map utilizando range.
--- exercicios/exercicios_nivel5/ex003/ex003.go
+++ exercicios/exercicios_nivel5/ex003/ex003.go
@@ -1,5 +1,35 @@
 package main
 
+import "fmt"
+
+type veiculo struct {
+	portas		int
+	cor 		string
+}
+
+type caminhonete struct {
+	veiculo
+	tracaoNasQuatro	bool
+}
+
+type sedan struct {
+	veiculo
+	modeloLuxo	bool
+}
+
 func main() {
-	
+	carroPasseio := sedan{veiculo{4, "Branco"}, true}
+	carroRoca := caminhonete{
+		veiculo: veiculo{
+			portas: 4,
+			cor: "Branco",
+		},
+		tracaoNasQuatro: true,
+	}
+
+	fmt.Println(carroPasseio)
+	fmt.Println(carroRoca)
+	fmt.Println(carroPasseio.cor)
+	fmt.Println(carroRoca.tracaoNasQuatro)
+
 }
